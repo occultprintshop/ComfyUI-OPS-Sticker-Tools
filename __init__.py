5436d8015c9681c42c0b01c287f974484614ebfd
occultprintshop <occultprintshop@gmail.com>
2026-08-28 05:37:29 +0100

    Enable smart sticker filtering v0.2.1

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from . import nodes as _nodes
+from .smart_sticker import install_smart_sticker_filter
+
+install_smart_sticker_filter(_nodes)
+
+NODE_CLASS_MAPPINGS = _nodes.NODE_CLASS_MAPPINGS
+NODE_DISPLAY_NAME_MAPPINGS = _nodes.NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
